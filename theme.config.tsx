557a3d9d7064f600439bd75cf13c465218fca492
Bai <baijiangjie@gmail.com>
2024-06-25 19:22:57 +0800

    perf: update docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>JumpServer</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/jumpserver/jumpserver',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com/invite/jcM5tKWJ',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/jumpserver/jumpserver',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'JumpServer Docs',
   },
 }
 
