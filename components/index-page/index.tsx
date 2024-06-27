9c0fb90595cba1ae189f3bec63afbf7ae7a4fc43
Jiangjie.Bai <bugatti_it@163.com>
2024-06-27 23:51:36 +0800

    perf: update index

--- components/index-page/index.tsx
+++ components/index-page/index.tsx
@@ -2,7 +2,7 @@ export function IndexPage() {
   return (
     <div className="index">
       <section className="text-center flex flex-col container items-center conf-block max-w-3xl">
-        <h2>A query language for your API</h2>
+        <h2>An open-source PAM tool (Basion Host)</h2>
         <p>
         </p>
       </section>
