d6718455458065d47715dffb4b9a70f76d27fe00
Bai <baijiangjie@gmail.com>
2024-06-29 00:21:56 +0800

    perf: update theme config

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,3 +1,4 @@
+import { DocsThemeConfig } from 'nextra-theme-docs';
 import { useRouter } from 'next/router'
 
 const capitalizeFirstLetter = (string) => {
@@ -8,17 +9,55 @@ const capitalizeFirstLetter = (string) => {
   return capitalizedWords.join(' ')
 }
 
-export default {
-  banner: {
-    key: '4.0-release',
-    text: (
-      <a href="https://github.com/jumpserver/jumpserver" target="_blank">
-        🎉 JumpServer 4.0 is released. Read more →
-      </a>
-    )
-  },
+const banner = {
+  key: '4.0-release',
+  text: (
+    <a href="https://github.com/jumpserver/jumpserver" target="_blank">
+      🎉 JumpServer 4.0 is released. Read more →
+    </a>
+  )
+}
+
+const logo = (
+  <>
+    <img 
+      src="/logo.svg" 
+      style={{ 
+        height: '36px',
+        maskImage: 'linear-gradient(60deg, black 25%, rgba(0, 0, 0, 0.2) 50%, black 75%)',
+        maskSize: '500%',
+        maskPosition: '0%',
+        transition: 'mask-position 1s ease'
+      }}
+      onMouseOver={(e) => e.currentTarget.style.maskPosition = '100%'}
+      onMouseOut={(e) => e.currentTarget.style.maskPosition = '0%'}
+    />
+  </>
+)
+
+const config: DocsThemeConfig = {
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="JumpServer" />
+      <meta property="og:description" content="An open-source PAM tool (Bastion Host)" />
+      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+    </>
+  ),
+  banner: banner,
+  logo: logo,
   project: {
     link: 'https://github.com/jumpserver/jumpserver',
+  },
+  chat: {
+    link: 'https://discord.com/invite/jcM5tKWJ',
+  },
+  feedback: {
+    useLink: () => 'https://github.com/jumpserver/jumpserver/issues/new?labels=%F0%9F%A4%94+Question&projects=&template=3_question.yml&title=%5BQuestion%5D+'
+  },
+  docsRepositoryBase: 'https://github.com/ibuler/jumpserver-documentation/tree/dev',
+  search: {
+    placeholder: 'Search...',
   },
   sidebar: {
     toggleButton: true,
@@ -31,13 +70,6 @@ export default {
       return <>{capitalizeFirstLetter(title)}</>
     }
   },
-  feedback: {
-    useLink: () => 'https://github.com/jumpserver/jumpserver/issues/new?labels=%F0%9F%A4%94+Question&projects=&template=3_question.yml&title=%5BQuestion%5D+'
-  },
-  chat: {
-    link: 'https://discord.com/invite/jcM5tKWJ',
-  },
-  docsRepositoryBase: 'https://github.com/ibuler/jumpserver-documentation/tree/dev',
   useNextSeoProps() {
     const { asPath } = useRouter()
     if (asPath == '/') {
@@ -50,38 +82,14 @@ export default {
       }
     }
   },
-  logo: (
-    <>
-      <img 
-        src="/logo.svg" 
-        style={{ 
-          height: '36px',
-          maskImage: 'linear-gradient(60deg, black 25%, rgba(0, 0, 0, 0.2) 50%, black 75%)',
-          maskSize: '500%',
-          maskPosition: '0%',
-          transition: 'mask-position 1s ease'
-        }}
-        onMouseOver={(e) => e.currentTarget.style.maskPosition = '100%'}
-        onMouseOut={(e) => e.currentTarget.style.maskPosition = '0%'}
-      />
-    </>
-  ),
-  head: (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="JumpServer" />
-      <meta property="og:description" content="An open-source PAM tool (Bastion Host)" />
-      <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-    </>
-  ),
   primaryHue: 150,
   primarySaturation: 50,
-  search: {
-    placeholder: 'Search...',
-  },
   navbar: {
   },
   darkMode: true,
+  toc: {
+    backToTop: true
+  },
   footer: {
     text: (
       <div className="flex w-full flex-col items-center sm:items-start">
@@ -91,7 +99,6 @@ export default {
       </div>
     )
   },
-  toc: {
-    backToTop: true
-  }
 }
+
+export default config
