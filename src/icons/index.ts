15f5117e615e7f17243edcf18841015846bd3293
Bai <baijiangjie@gmail.com>
2024-06-30 00:23:02 +0800

    perf: add bottom of footer

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,2 +1,5 @@
 export { default as JumpServerFavIcon } from './favicon.svg'
 export { default as JumpServerWordmarkLogoIcon } from './jumpserver.svg'
+export { default as DiscordIcon } from "./discord.svg"
+export { default as GitHubIcon } from "./github.svg"
+export { default as TwitterIcon } from "./twitter.svg"
