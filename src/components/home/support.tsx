12c1a91b76b17c60a923753ee1c3ffef3c738f3c
Bai <baijiangjie@gmail.com>
2024-07-05 22:52:29 +0800

    refactor: home add Support Services

--- src/components/home/support.tsx
+++ src/components/home/support.tsx
@@ -1,7 +1,64 @@
+import * as React from 'react';
+
+function createData(
+  name: string,
+  content: string,
+) {
+  return { name, content };
+}
+
+const rows = [
+  createData('Customer Support ', '8x5 ticket and telephone support, tickets responding in 4 hours. After receiving the fault report, the engineer assists the customer in software faults troubleshooting in time through telephone support, remote access, etc.'),
+  createData('Installation and Training', 'Professional support (such as installation, emergency assistance, software troubleshooting, training, etc.)'),
+  createData('Emergency', ''),
+  createData('Software Upgrade', 'Provide patches and enhanced function packages to upgrade seamlessly, etc.'),
+  createData('Online Self-service', 'The customer support portal keeps customers posted with the latest software features, maintenance experience, use skills, and any other related knowledge.'),
+];
+
+function TableSupport() {
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Service Content</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Customer Support</td>
+            <td>8x5 ticket and telephone support, tickets responding in 4 hours. After receiving the fault report, the engineer assists the customer in software faults troubleshooting in time through telephone support, remote access, etc.</td>
+          </tr>
+          <tr>
+            <td>Installation and Training</td>
+            <td rowSpan={2}>Professional support (such as installation, emergency assistance, software troubleshooting, training, etc.)</td>
+          </tr>
+          <tr>
+            <td>Emergency</td>
+          </tr>
+          <tr>
+            <td>Software Upgrade</td>
+            <td>Provide patches and enhanced function packages to upgrade seamlessly, etc.</td>
+          </tr>
+          <tr>
+            <td>Online Self-service</td>
+            <td>The customer support portal keeps customers posted with the latest software features, maintenance experience, use skills, and any other related knowledge.</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+
 export default function Support() {
   return (
     <>
-      <h1>Support</h1>
+      <section>
+        <h2>Enterprise Support Services</h2>
+        <TableSupport />
+      </section>
     </>
   )
 }
