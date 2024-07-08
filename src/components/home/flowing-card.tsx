9073bf59aa5fb6a366a35d0e175edb6abc5b4788
Bai <baijiangjie@gmail.com>
2024-07-08 11:08:59 +0800

    home: update moving images

--- src/components/home/flowing-card.tsx
+++ src/components/home/flowing-card.tsx
@@ -9,10 +9,10 @@ const Moving = () => {
           speed="normal"
           className="mx-auto [&_pre]:h-[calc(100%-48px)] [&_div.nextra-code]:h-full"
         >
-          <ImageComponent src="/images/home/system-settings.png" alt="SystemSettings" />
-          <ImageComponent src="/images/home/workbench.png" alt="Workbench" />
-          <ImageComponent src="/images/home/audits.png" alt="Audits" />
-          <ImageComponent src="/images/home/console.png" alt="Console" />
+          <ImageComponent src="/images/home/moving-system-settings.png" alt="SystemSettings" />
+          <ImageComponent src="/images/home/moving-workbench.png" alt="Workbench" />
+          <ImageComponent src="/images/home/moving-audits.png" alt="Audits" />
+          <ImageComponent src="/images/home/moving-console.png" alt="Console" />
         </InfiniteMovingCards>
       </div>
     </>
