bc3cf7521e82f6fd2315977db155534a2f39e2d2
Bai <baijiangjie@gmail.com>
2024-07-08 12:06:51 +0800

    home: update

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,11 +1,9 @@
 import Hero from './hero'
 import FlowingCard from './flowing-card'
 import WhatsJumpServer from './whats'
-import Advantages from './advantages'
 import XPack from './x-pack'
 import Support from './support'
 import Edition from './edition'
-import ContactUs from "./contact-us"
 
 function HomePage() {
   return (
