3729b1a37f37957ace64d209743c1c4cef297954
Bai <baijiangjie@gmail.com>
2024-07-26 19:13:52 +0800

    perf: google-analytics use packages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,13 @@
 import "tailwindcss/tailwind.css";
 import "@/styles/globals.css";
-import GoogleAnalytics from "./google-analytics";
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 
 // This default export is required in a new `pages/_app.js` file.
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <GoogleAnalytics />
+      <GoogleAnalytics gaId="G-QJE3MCHN3H"/>
       <Component {...pageProps} />
     </>
   );
