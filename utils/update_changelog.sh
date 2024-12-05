9091d4a87106c666eae9d5383a4eaf15e073e489
Bai <baijiangjie@gmail.com>
2024-12-05 18:30:37 +0800

    perf: add uitls

--- utils/update_changelog.sh
+++ utils/update_changelog.sh
@@ -7,9 +7,6 @@ FILE_PATH="../src/components/docs/changelog.tsx"
 
 sed -i "2i\\${CONTENT}" "${FILE_PATH}"
 
-git config --global user.name 'BaiJiangJie'
-git config --global user.email 'jiangjie.bai@fit2cloud.com'
-
 git add ${FILE_PATH}
 git commit -m "add $TAG_NAME to changelogs"
 git checkout -b pr@dev@changelog
