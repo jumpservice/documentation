b0a53541ba28f776cc399fd2dad09ac8f2ac7856
Bai <baijiangjie@gmail.com>
2024-12-20 17:51:17 +0800

    fix: auto change latest_version in utils

--- utils/update_changelog.sh
+++ utils/update_changelog.sh
@@ -4,16 +4,17 @@
 DATE=$(date +%Y-%m-%d)
 
 # changelog
-CONTENT="    { date: '$DATE', version: '$TAG_NAME'},"
-FILE_PATH="../src/components/docs/changelog.tsx"
-sed -i "2i\\${CONTENT}" "${FILE_PATH}"
+CHANGELOG_CONTENT="    { date: '$DATE', version: '$TAG_NAME'},"
+CHANGELOG_FILE_PATH="../src/components/docs/changelog.tsx"
+sed -i "2i\\${CHANGELOG_CONTENT}" "${CHANGELOG_FILE_PATH}"
 
 # last version
-CONTENT="const LatestVersion = '$TAG_NAME';"
-FILE_PATH="../src/components/public/version.tsx"
-sed -i "1s/.*/${CONTENT}/" "${FILE_PATH}"
+VERSION_CONTENT="const LatestVersion = '$TAG_NAME';"
+VERSION_FILE_PATH="../src/components/public/version.tsx"
+sed -i "1s/.*/${VERSION_CONTENT}/" "${VERSION_FILE_PATH}"
 
-git add ${FILE_PATH}
+git add ${CHANGELOG_FILE_PATH}
+git add ${VERSION_FILE_PATH}
 git commit -m "add $TAG_NAME to changelogs"
 git checkout -b pr@dev@changelog
 git push origin pr@dev@changelog
