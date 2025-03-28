b8bd804d4019750ef3eef05bbcd760b3a54740a3
Bai <baijiangjie@gmail.com>
2025-03-28 17:06:50 +0800

    perf: architecture

--- src/components/home/architecture.tsx
+++ src/components/home/architecture.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { JumpServerWordmarkLogoIcon } from "@/icons";
-import { ImageComponent } from "../public";
-import ArrowLine from "../public/arrow-line";
 
 const assets = [
   { name: "SSH", logo: "/svgs/linux.svg" },
@@ -23,21 +21,20 @@ const users = [
   { name: "DBA", logo: "/svgs/dba.svg" },
   { name: "Developer", logo: "/svgs/developer.svg" },
   { name: "QA", logo: "/svgs/qa.svg" },
-  { name: "...", logo: "/svgs/more.svg" },
+  { name: "Other...", logo: "/svgs/more.svg" },
 ];
-
 
 const ArchitectureDiagram = () => {
   return (
-    <div className="max-w-4xl mx-auto p-6 rounded-lg shadow-lg shadow-primary">
+    <div className="max-w-6xl mx-auto p-6 rounded-lg shadow-md shadow-primary/90">
 
-      <div className="border border-gray-500 dark:border-gray-600 rounded-lg p-4">
-        <div className="text-2xl font-semibold text-center mb-10">IT Assets</div>
-        <div className="grid grid-cols-5 gap-4 text-center">
+      <div className="border border-gray-500 dark:border-gray-400 rounded-lg p-4">
+        <div className="text-xl font-serif text-center mb-6">IT Assets</div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4 text-center">
           {assets.map((item) => (
-            <div key={item.name} className="flex flex-col items-center p-4 border border-black dark:border-gray-600 rounded-lg">
-              <div className="w-8 h-8 flex items-center justify-center">
-                <img src={item.logo} alt="x" />
+            <div key={item.name} className="flex flex-col items-center p-4 border border-black dark:border-gray-400 rounded-lg">
+              <div className="w-8 h-8">
+                <img src={item.logo} alt={item.name} className="max-w-full h-auto" />
               </div>
               <div className="mt-2 text-sm font-semibold">{item.name}</div>
             </div>
@@ -46,61 +43,58 @@ const ArchitectureDiagram = () => {
       </div>
 
       <div className="relative flex h-20 justify-center items-center">
-        <svg width="300" height="200" className="text-black dark:text-white">
+        <svg width="100%" height="110" className="text-black dark:text-white">
           <defs>
-            <marker id="arrowhead" markerWidth="10" markerHeight="7" refX="7" refY="3.5" orient="auto" >
-              <polygon points="0 0, 10 3.5, 0 7" fill="black" className="dark:fill-gray-400 dark:stroke-gray-400" />
+            <marker id="arrowhead" markerWidth="8" markerHeight="6" refX="6" refY="3" orient="auto">
+              <polygon points="0 0, 8 3, 0 6" fill="currentColor" />
             </marker>
           </defs>
-          <line x1="150" y1="135" x2="150" y2="70" stroke="black" stroke-width="1" stroke-dasharray="5,5" className="dark:stroke-gray-400" markerEnd="url(#arrowhead)" />
-          <line x1="150" y1="135" x2="20" y2="70" stroke="black" strokeWidth="1" stroke-dasharray="5,5" className="dark:stroke-gray-400" markerEnd="url(#arrowhead)" />
-          <line x1="150" y1="135" x2="280" y2="70" stroke="black" strokeWidth="1" stroke-dasharray="5,5" className="dark:stroke-gray-400" markerEnd="url(#arrowhead)" />
+          <line x1="50%" y1="80" x2="50%" y2="30" stroke="currentColor" strokeWidth="1" strokeDasharray="5,5" markerEnd="url(#arrowhead)" />
+          <line x1="50%" y1="80" x2="10%" y2="30" stroke="currentColor" strokeWidth="1" strokeDasharray="5,5" markerEnd="url(#arrowhead)" />
+          <line x1="50%" y1="80" x2="90%" y2="30" stroke="currentColor" strokeWidth="1" strokeDasharray="5,5" markerEnd="url(#arrowhead)" />
         </svg>
       </div>
 
-      <div className="border border-primary rounded-lg p-4">
-        <div className="flex w-full">
-          <div className="flex flex-1 justify-end pr-4">
-            <JumpServerWordmarkLogoIcon className="theme-logo h-9" />
-          </div>
-          <div className="flex flex-1 justify-start pl-4">
-            <div className="flex flex-col text-primary font-semibold text-md">
-              <span className="font-bold">An open-source PAM tool</span>
-              <span className="font-bold">( Bastion Host )</span>
+      <div className="border border-primary rounded-lg p-4 shadow-[0_0_10px_3px] shadow-primary">
+        <div className="theme-logo">
+          <div className="flex w-full flex-wrap justify-center gap-4">
+            <JumpServerWordmarkLogoIcon className="h-9" />
+            <div className="text-primary font-semibold text-md text-center">
+              <span className="block font-bold">An open-source PAM tool</span>
+              <span className="block font-bold">( Bastion Host )</span>
             </div>
           </div>
-        </div>
-
-        <div className="grid grid-cols-4 gap-4 text-center text-primary mt-4">
-          {securityLayers.map((item) => (
-            <div key={item.name} className="flex flex-col items-center p-4 border border-primary rounded-lg shadow">
-              <div className="w-8 h-8 flex items-center justify-center">
-                <img src={item.logo} alt="x" className=""/>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 text-center text-primary mt-4">
+            {securityLayers.map((item) => (
+              <div key={item.name} className="flex flex-col items-center p-4 border border-primary rounded-lg shadow">
+                <div className="w-8 h-8">
+                  <img src={item.logo} alt={item.name} className="max-w-full h-auto" />
+                </div>
+                <div className="mt-2 text-md font-bold">{item.name}</div>
               </div>
-              <div className="mt-2 text-md font-bold">{item.name}</div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
 
       <div className="p-4 pt-0">
-        <div className="grid grid-cols-5 gap-4 text-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4 text-center">
           {users.map((item) => (
-            <div>
-              <div className="relative flex h-20 justify-center items-center">
-                <svg width="100" height="200" className="text-black dark:text-white">
+            <div key={item.name} className="flex flex-col items-center">
+              <div className="relative flex h-18 justify-center items-center">
+                <svg width="100%" height="80" className="text-black dark:text-white">
                   <defs>
-                    <marker id="arrowhead" markerWidth="10" markerHeight="7" refX="7" refY="3.5" orient="auto" >
-                      <polygon points="0 0, 10 3.5, 0 7" fill="black" className="dark:fill-gray-400 dark:stroke-gray-400" />
+                    <marker id="user-arrowhead" markerWidth="8" markerHeight="6" refX="6" refY="3" orient="auto">
+                      <polygon points="0 0, 8 3, 0 6" fill="currentColor" />
                     </marker>
                   </defs>
-                  <line x1="50" y1="135" x2="50" y2="70" stroke="black" stroke-width="1" stroke-dasharray="5,5" className="dark:stroke-gray-400" markerEnd="url(#arrowhead)" />
+                  <line x1="50%" y1="80" x2="50%" y2="30" stroke="currentColor" strokeWidth="1" strokeDasharray="5,5" markerEnd="url(#user-arrowhead)" />
                 </svg>
               </div>
 
-              <div key={item.name} className="flex flex-col items-center p-4 border border-gray-500 dark:border-gray-600 rounded-lg shadow">
-                <div className="w-8 h-8 flex items-center justify-center">
-                  <img src={item.logo} alt="x" className=""/>
+              <div className="p-4 flex flex-col items-center rounded-lg">
+                <div className="w-8 h-8">
+                  <img src={item.logo} alt={item.name} className="max-w-full h-auto" />
                 </div>
                 <div className="mt-2 text-sm font-semibold">{item.name}</div>
               </div>
@@ -114,20 +108,12 @@ const ArchitectureDiagram = () => {
 
 function Architecture() {
   return (
-    <>
-      <div className="">
-        <section>
-          <h2>JumpServer Architecture</h2>
-          <ArchitectureDiagram />
-          <ImageComponent
-            src="/images/jumpserver-overview.png"
-            alt="jumpserver-overview"
-            width={1000}
-            height={800}
-          />
-        </section>
-      </div>
-    </>
+    <div className="max-w-6xl mx-auto px-4">
+      <section>
+        <h2 className="text-xl font-bold text-center my-6">JumpServer Architecture</h2>
+        <ArchitectureDiagram />
+      </section>
+    </div>
   );
 }
 
