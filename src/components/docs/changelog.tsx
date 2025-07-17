627dd3e5eea13f49728609bff8ddbc6805c9caa7
BaiJiangJie <jiangjie.bai@fit2cloud.com>
2025-07-17 10:10:00 +0000

    add v4.10.4 to changelogs

--- src/components/docs/changelog.tsx
+++ src/components/docs/changelog.tsx
@@ -1,4 +1,5 @@
 export const changelogData = [
+    { date: '2025-07-17', version: 'v4.10.4'},
     { date: '2025-06-30', version: 'v4.10.3'},
     { date: '2025-06-19', version: 'v4.10.2'},
     { date: '2025-05-19', version: 'v4.10.1'},
