d96943834e41b9906b3b5cc5332381f9779d83c1
Bai <baijiangjie@gmail.com>
2025-08-11 15:45:52 +0800

    perf: a little

--- src/components/docs/steps.tsx
+++ src/components/docs/steps.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 
 const Steps = ({ children, stepLevel = "1"}: { children: React.ReactNode, stepLevel: string}) => {
+  const stepClassMapper = {
+    "1": "mt-5",
+    "2": ""
+  };
   const stepIndexClassMapper = {
     "1": "w-5 h-5 text-sm mt-1 font-bold bg-primary text-white",
     "2": "w-5 h-5 text-sm mt-1 font-bold bg-black text-white dark:bg-[#E7EDF2] dark:text-black",
   }
   return (
-    <div className={`steps flex flex-col gap-1`}>
+    <div className={`steps flex flex-col gap-1 ${stepClassMapper[stepLevel]}`}>
       {React.Children.map(children, (child, index) => (
         <div className="flex gap-2 mt-3">
           <div className={ 
