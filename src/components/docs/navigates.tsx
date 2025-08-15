d6a6696c3b6c58fbdb89f627c25a0f4dd8056aea
Bai <baijiangjie@gmail.com>
2025-08-15 17:22:15 +0800

    perf: create user

--- src/components/docs/navigates.tsx
+++ src/components/docs/navigates.tsx
@@ -3,7 +3,7 @@ import Icon from './icons'
 const NavConsole = () => {
   return (
     <div>
-      <p>In the upper-left of the page, select <Icon name="Switch" />, then click <Icon name="Console" /> <b>Console</b>.</p>
+      <p>At the bottom-left of the top navigation bar, select <Icon name="Switch" />, then click <Icon name="Console" /> <b>Console</b>.</p>
       <img src="/images/docs/navs/nav-console.png" alt="image" width="300"/>
     </div>
   );
@@ -12,7 +12,7 @@ const NavConsole = () => {
 const NavPAM = () => {
   return (
     <div>
-      <p>In the upper-left of the page, select <Icon name="Switch" />, then click <Icon name="pam" /> <b>PAM</b>.</p>
+      <p>At the bottom-left of the top navigation bar, select <Icon name="Switch" />, then click <Icon name="pam" /> <b>PAM</b>.</p>
       <img src="/images/docs/navs/nav-pam.png" alt="image" width="300"/>
     </div>
   );
@@ -21,7 +21,7 @@ const NavPAM = () => {
 const NavAudits = () => {
   return (
     <div>
-      <p>In the upper-left of the page, select <Icon name="switch" />, then click <Icon name="audit" /> <b>Audits</b>.</p>
+      <p>At the bottom-left of the top navigation bar, select <Icon name="switch" />, then click <Icon name="audit" /> <b>Audits</b>.</p>
       <img src="/images/docs/navs/nav-audits.png" alt="image" width="300"/>
     </div>
   );
@@ -30,7 +30,7 @@ const NavAudits = () => {
 const NavWebTerminal = () => {
   return (
     <div>
-      <p>In the upper-right of the page, click <Icon name="square-terminal"/>.</p>
+      <p>In the right area of the top navigation bar, click <Icon name="square-terminal"/>.</p>
       <img src="/images/docs/navs/nav-web-terminal.png" alt="image" width="350"/>
     </div>
   )
@@ -40,7 +40,7 @@ const NavWebTerminal = () => {
 const NavSystemSettings = () => {
   return (
     <div>
-      <p>In the upper-right of the page, click <Icon name="settings"/>.</p>
+      <p>In the right area of the top navigation bar, click <Icon name="settings"/>.</p>
       <img src="/images/docs/navs/nav-settings.png" alt="image" width="350"/>
     </div>
   );
