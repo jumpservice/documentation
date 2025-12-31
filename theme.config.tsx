11a43b18364a0a0ce8e3f454f6774e382d932e65
BugKing9527 <wangzhen@fit2cloud.com>
2025-12-31 15:00:25 +0800

    chore: modify some links

--- theme.config.tsx
+++ theme.config.tsx
@@ -29,7 +29,6 @@ export const LinkTitle: React.FC<LinkTitleProps> = ({ title, url }) => {
   );
 };
 
-
 const config: DocsThemeConfig = {
   components: { 
     Icon, Alert, Cards, Card,
@@ -51,6 +50,7 @@ const config: DocsThemeConfig = {
     </>
   ),
   logo: Logo,
+  logoLink: "https://www.jumpserver.com/",
   feedback: {
     useLink: () =>
       "https://github.com/jumpserver/jumpserver/issues/new?labels=%F0%9F%A4%94+Question&projects=&template=3_question.yml&title=%5BQuestion%5D+",
