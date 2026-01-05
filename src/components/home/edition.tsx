c6463a22c87ae716f86c4c16fd7fa03d46d95c0d
wan92hen <wangzhen@fit2cloud.com>
2026-01-05 11:26:12 +0800

    chore: refine edition table

--- src/components/home/edition.tsx
+++ src/components/home/edition.tsx
@@ -22,14 +22,12 @@ function TableSupport() {
               <td>50</td>
               <td>Standalone</td>
               <td>Annual Subscription</td>
-              <td className="border-l dark:border-l dark:border-gray-700">
+              <td rowSpan={4} className="border-l dark:border-l dark:border-gray-700">
                 <div className="flex flex-col gap-2">
-                  <a href="https://www.lxware.hk/products/jumpserver-enterprise-edition-basic-plan" target="_blank" className="hover:underline hover:text-primary">
-                    Buy Now <Icon name="link" />
-                  </a>
                   <a href="https://aws.amazon.com/marketplace/pp/prodview-vapdbpzpdbjam" target="_blank" className="hover:underline hover:text-primary">
                     Buy With AWS <Icon name="link" />
                   </a>
+                  <a href="mailto:sales@lxware.hk" className="hover:underline hover:text-primary">Contact Sales <Icon name="send-email" /></a>
                 </div>
               </td>
             </tr>
@@ -38,9 +36,6 @@ function TableSupport() {
               <td>500</td>
               <td>Standalone | Active-Standby</td>
               <td>Annual Subscription</td>
-              <td rowSpan={3} className="border-l dark:border-l dark:border-gray-700">
-                <a href="mailto:sales@lxware.hk" className="hover:underline hover:text-primary">Contact Sales <Icon name="send-email" /></a>
-              </td>
             </tr>
             <tr>
               <th scope="row">Professional</th>
